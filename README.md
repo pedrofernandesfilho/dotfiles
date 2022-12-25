b7f9bebe32726380e5f5a7b09fef9fa8f52c9d69
Pedro Fernandes Filho <pedro.fernandesfilho@gmail.com>
2022-12-25 20:04:30 -0300

    Organiza arquivos
    Cria .gitignore
    Atualiza README
    Adiciona arquivos customizados para oh-my-zsh
    	aliases
    	enviroment-vars
    Cria arquivo de instalação
    	pra criar symlinks para oh-my-zsh

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # dotfiles
+
+## Pré requisitos
+
+- Git
+
+## Configuraçõas para
+- Tmux
+- oh-my-zsh
+
+## Instalação
+
+```bash
+cd ~
+git init -b main
+git remote add origin git@github.com:pedrofernandesfilho/dotfiles.git
+git pull origin main
+git push -u origin main
+```
+
+## Infos
+
+O diretório `dotfiles` contém atalhos (symlinks) para arquivos com a finalidade de facilitar a edição e manutenção dos mesmos.
---
+++ dotfiles/oh-my-zsh-custom/aliases.zsh
@@ -0,0 +1,37 @@
+# Docker
+
+alias dk='docker'
+alias dkc='docker-compose'
+alias dkpsa='docker ps -a'
+alias dks='sudo systemctl start docker.service'
+
+# DOTNET
+
+alias dn='dotnet'
+alias dnb='dotnet build'
+alias dnbni='dotnet build --no-incremental'
+alias dni='dotnet --info'
+alias dnn='dotnet new'
+alias dnre='dotnet restore'
+alias dnt='dotnet test'
+
+# Git
+
+alias gadd='git add'
+alias gbranch='git branch'
+alias gco='git checkout'
+alias gcob='git checkout -b'
+alias gctv='git commit -v'
+alias gctav='git commit --amend -v'
+alias gctsv='git commit -S -v'
+alias gdif='git diff'
+alias gdiff='git diff'
+alias gfetch='git fetch'
+alias gk='LANG=en_US gitk'
+alias glogo='git log --oneline'
+alias gpull='git pull'
+alias gpullr='git pull -r'
+alias gpush='git push'
+alias gstash='git stash'
+alias gstashl='git stash list'
+alias gstashp='git stash pop'
