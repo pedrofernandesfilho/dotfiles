0fa497c501fdc470b7771667e2aa41d5b5a6370f
Pedro Fernandes Filho <pedro.fernandesfilho@gmail.com>
2023-01-02 19:24:32 -0300

    Cria variáveis de ambiente para Android Studio

--- dotfiles/oh-my-zsh-custom/environment-vars.zsh
+++ dotfiles/oh-my-zsh-custom/environment-vars.zsh
@@ -1,3 +1,11 @@
 export GPG_TTY=$(tty)
+
+
+# DOTNET
 export PATH="$HOME/.dotnet:$PATH"
 export DOTNET_CLI_UI_LANGUAGE="en"
+
+# ANDROID
+export ANDROID_SDK_ROOT=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
+export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
