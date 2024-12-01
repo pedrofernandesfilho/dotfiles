7066874b1172de1c45e08a6bd080628e9178f9b6
Pedro Fernandes Filho <pedro.fernandesfilho@gmail.com>
2024-12-01 19:46:16 -0300

    Commenta alias personalizados
    Comenta variáveis de ambiente não utilizadas

--- oh-my-zsh-custom/environment-vars.zsh
+++ oh-my-zsh-custom/environment-vars.zsh
@@ -9,10 +9,10 @@ export DOTNET_CLI_UI_LANGUAGE="en"
 
 # ANDROID
 
-export ANDROID_HOME=$HOME/Android/Sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+# export ANDROID_HOME=$HOME/Android/Sdk
+# export PATH=$PATH:$ANDROID_HOME/emulator
+# export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 # JAVA
 
-#export JAVA_HOME=
+# export JAVA_HOME=
