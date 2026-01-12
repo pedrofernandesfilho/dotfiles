485859adc17f88e296b3efb556230afe97026c26
Pedro Fernandes Filho <pedro.fernandesfilho@gmail.com>
2026-01-12 18:46:11 -0300

    Configura env var JAVA_HOME com asdf

--- oh-my-zsh-custom/environment-vars.zsh
+++ oh-my-zsh-custom/environment-vars.zsh
@@ -2,17 +2,27 @@
 
 export GPG_TTY=$(tty)
 
+
 # DOTNET
 
-export PATH="$HOME/.dotnet:$PATH"
+if [[ -d "$HOME/.dotnet" ]]; then
+    export PATH="$HOME/.dotnet:$PATH"
+fi
 export DOTNET_CLI_UI_LANGUAGE="en"
 
-# ANDROID
-
-# export ANDROID_HOME=$HOME/Android/Sdk
-# export PATH=$PATH:$ANDROID_HOME/emulator
-# export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 # JAVA
 
-# export JAVA_HOME=
+# Set JAVA_HOME via asdf if it is available
+if command -v asdf &> /dev/null; then
+    export JAVA_HOME="$(asdf where java 2>/dev/null)"
+fi
+
+
+# ANDROID
+
+# if [[ -d "$HOME/Android/Sdk" ]]; then
+#     export ANDROID_HOME="$HOME/Android/Sdk"
+#     export PATH="$PATH:$ANDROID_HOME/emulator"
+#     export PATH="$PATH:$ANDROID_HOME/platform-tools"
+# fi
